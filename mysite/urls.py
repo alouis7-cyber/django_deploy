f404619134861ccae55b1cdb035666cfc28990ed
alouis7 cyber <alouis7@ivytech.edu>
2026-04-21 23:37:41 -0400

    Fix WSGI path and cleanup

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,1 @@
-from django.urls import include, path
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("", lambda request: HttpResponse("Your Django app is running!")),
-
-]
+from django.http import HttpResponse

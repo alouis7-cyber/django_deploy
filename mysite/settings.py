90f3e8d436b041780a454720b4a21b8578190a95
alouis7 cyber <alouis7@ivytech.edu>
2026-04-21 22:19:09 -0400

    Initial Django project

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+import os
+import dj_database_url
+
+ALLOWED_HOSTS = ["*"]
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,3 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
+    path("", lambda request: HttpResponse("Your Django app is running!")),
+
 ]
